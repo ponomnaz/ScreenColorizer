31b26211bc02f4ba1b6330f288d4cb4aeccf7acd
ponomnaz <ponomarev.naz@gmail.com>
2025-11-24 14:09:30 +0300

    main

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from SelectorProcessor import SelectorProcessor
 from TreeBuilder import TreeBuilder
 
 if __name__ == "__main__":
-    SelectorProcessor().process()
-    # input_file = 'data/example.txt'  # Путь к входному файлу
-    # output_dir_tree = 'data/tree.txt'
-    # output_dir_all = 'data/all_paths.txt'
-    # TreeBuilder.build_tree_from_file(input_file, output_dir_tree)
-    # TreeBuilder.extract_all_paths(input_file, output_dir_all)
+   #SelectorProcessor().process()
+    input_file = 'data/example.txt'  # Путь к входному файлу
+    output_dir_tree = 'data/tree.txt'
+    output_dir_all = 'data/all_paths.txt'
+    TreeBuilder.build_tree_from_file(input_file, output_dir_tree)
+    TreeBuilder.extract_all_paths(input_file, output_dir_all)
